2adfff2847bfa2ac807e03b7aaef13df417cd42f
Rasel <103592581+alfred-rasel@users.noreply.github.com>
2023-09-26 22:51:40 +0600

    create a server and multiple client that interact each other

---
+++ src/Socket/Information.java
@@ -0,0 +1,12 @@
+package Socket;
+
+
+public class Information {
+    public String username;
+    public NetworkConnection netConnection;
+    public Information(String user, NetworkConnection nConnection){
+        username=user;
+        netConnection=nConnection;
+    }
+
+}
